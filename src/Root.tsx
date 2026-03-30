a2996ef8b84fc906cf282443e18c9a0f6e53a257
Victor Marinho <victorcmarinho@gmail.com.br>
2026-03-30 01:18:02 -0300

    refactor: move default cinematic configuration directly into RemotionRoot composition props

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,6 +1,6 @@
 import { Composition } from 'remotion';
 import { FullJourneySequence } from './scenes/FullJourneySequence';
-import { defaultCinematicConfig, cinematicSchema } from './config/cinematicConfig';
+import { cinematicSchema } from './config/cinematicConfig';
 import './index.css';
 
 export const RemotionRoot: React.FC = () => {
@@ -8,13 +8,32 @@ export const RemotionRoot: React.FC = () => {
     <>
       <Composition
         id="CinematicPortfolio"
-        component={FullJourneySequence as React.FC<any>}
-        durationInFrames={defaultCinematicConfig.globalDuration}
-        fps={defaultCinematicConfig.fps}
-        width={defaultCinematicConfig.width}
-        height={defaultCinematicConfig.height}
-        defaultProps={defaultCinematicConfig}
+        component={FullJourneySequence}
+        durationInFrames={3000}
+        fps={60}
+        width={3840}
+        height={2160}
         schema={cinematicSchema}
+        defaultProps={{
+          fps: 60,
+          globalDuration: 3000,
+          width: 3840,
+          height: 2160,
+          scenes: {
+            hero: { startFrame: 0, durationInFrames: 480, scrollYOffset: 140 },
+            about: { startFrame: 600, durationInFrames: 240, scrollYOffset: -549 },
+            experience: { startFrame: 960, durationInFrames: 360, scrollYOffset: -1438 },
+            skills: { startFrame: 1440, durationInFrames: 240, scrollYOffset: -2277 },
+            projects: { startFrame: 1800, durationInFrames: 360, scrollYOffset: -3066 },
+            footer: { startFrame: 2280, durationInFrames: 180, scrollYOffset: -3780 },
+          },
+          effects: {
+            baseScale: 2.0,
+            zoomOutScale: 0.8,
+            zoomOutStartFrame: 2580,
+            finalScrollY: -2200,
+          }
+        }}
       />
     </>
   );
--- src/config/cinematicConfig.ts
+++ src/config/cinematicConfig.ts
@@ -32,26 +32,3 @@ export const cinematicSchema = z.object({
 });
 
 export type CinematicProps = z.infer<typeof cinematicSchema>;
-
-export const defaultCinematicConfig: CinematicProps = {
-  fps: 60,
-  globalDuration: 3000,
-  width: 3840,
-  height: 2160,
-
-  scenes: {
-    hero: { startFrame: 0, durationInFrames: 480, scrollYOffset: 140 },
-    about: { startFrame: 600, durationInFrames: 240, scrollYOffset: -549 },
-    experience: { startFrame: 960, durationInFrames: 360, scrollYOffset: -1438 },
-    skills: { startFrame: 1440, durationInFrames: 240, scrollYOffset: -2277 },
-    projects: { startFrame: 1800, durationInFrames: 360, scrollYOffset: -3066 },
-    footer: { startFrame: 2280, durationInFrames: 180, scrollYOffset: -3780 },
-  },
-
-  effects: {
-    baseScale: 2.0,
-    zoomOutScale: 0.8,
-    zoomOutStartFrame: 2580,
-    finalScrollY: -2200
-  }
-};
