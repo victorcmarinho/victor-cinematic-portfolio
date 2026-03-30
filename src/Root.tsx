468b1f68ec3f32467015d95d47ba088ac27aabb1
Victor Marinho <victorcmarinho@gmail.com.br>
2026-03-30 00:28:46 -0300

    chore: remove remotion-best-practices agent skills and unused scene components

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,20 +1,17 @@
 import { Composition } from 'remotion';
-import { MainScene } from './scenes/MainScene';
+import { MyComposition } from './Composition';
 import './index.css';
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
       <Composition
-        id="PortfolioAppleStyle"
-        component={MainScene}
-        durationInFrames={1200}
+        id="CinematicPortfolio"
+        component={MyComposition}
+        durationInFrames={720}
         fps={60}
         width={3840}
         height={2160}
-        defaultProps={{
-          profileImg: "https://victorcmarinho.app/perfil.webp"
-        }}
       />
     </>
   );
